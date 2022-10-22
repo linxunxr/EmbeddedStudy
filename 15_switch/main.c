d36ae82df91a6c91900ca5abb2f7925a44aa968b
linxunxr <liulinxr@yeah.net>
2022-10-22 15:18:03 +0800

    switch语句

---
+++ 15_switch/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+// switch语句
+int main()
+{
+    int num = 3;
+
+    switch (num) {
+    case 1:
+        printf("1111111\n");
+        break;
+    case 2:
+        printf("222222222\n");
+        break;
+    case 3:
+        printf("333333333\n");
+        break;
+    default:
+        printf("xxxxxxxx\n");
+        break;
+    }
+    return 0;
+}
