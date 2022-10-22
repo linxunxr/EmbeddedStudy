a8f8fb1c16aa8b181247575f13682bd2394f6fb1
linxunxr <liulinxr@yeah.net>
2022-10-22 15:22:20 +0800

    for循环

---
+++ 16_for/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// for循环
+int main()
+{
+    int i;
+    int sum = 0;
+    for (i = 1;i <= 100; i++) {
+        sum += i;
+    }
+    printf("1 + 2 + 3 + ... + 100 = %d\n",sum);
+    return 0;
+}
