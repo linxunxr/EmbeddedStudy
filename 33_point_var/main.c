16f45d89c52172cd5db24560a9cb316b0931f012
林讯 <liulinxr@yeah.net>
2022-10-29 10:57:09 +0800

    指针与变量

---
+++ 33_point_var/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int *p1,*p2,temp,a,b;
+    p1 = &a;
+    p2 = &b;
+
+    printf("请输入a,b的值\n");
+    scanf("%d %d",p1,p2);
+    // 交换ab的值
+    temp = *p1;
+    *p1 = *p2;
+    *p2 = temp;
+
+    printf("a = %d b = %d\n",a,b);
+    printf("*p1 = %d *p2 = %d\n",*p1,*p2);
+    return 0;
+}
