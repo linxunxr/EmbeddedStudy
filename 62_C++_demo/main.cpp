ccee2fcf6851b6ffe1a86d09653fb3d0935af075
linxunPC <liulinxr@foxmail.com>
2022-12-28 17:19:46 +0800

    第一个C++程序

---
+++ 62_C++_demo/main.cpp
@@ -0,0 +1,12 @@
+// 标准的输入输出流
+#include <iostream>
+
+// 声明一个命名空间 cout对象存在std作用域下
+using namespace std;
+
+int main() {
+    // cout 标准输出流
+    // endl 换行
+    cout << "Hello, World!" << endl;
+    return 0;
+}
